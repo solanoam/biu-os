bc4bec4a95acf4f82bdb9b131619dfde1b675f18
solanoam <noam5456@gmail.com>
2018-10-28 09:20:44 +0200

    argc is working

---
+++ ex1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/types.h>
+#define MAX_NUM_FILES 3
+#define MAX_FILE_NAME 20
+
+int main(int argc, char **argv) {
+    int fdin1;
+    int fdin2;
+    //char[MAX_FILE_NAME] tempName;
+    if (argc != MAX_NUM_FILES) {
+        printf("Please pass two files as arguments. Try again");
+        return 0;
+    }
+
+    return 0;
+}
