04c8b20f6126b10408a0c38cca1d7bfff4efcabc
solanoam <noam5456@gmail.com>
2018-11-01 17:38:30 +0200

    updating gitignore

--- ex1/main.c
+++ ex1/main.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/types.h>
 #define MAX_ARG 3
-#define MAX_FILE_NAME 20
 
 
 //closing all open fd
@@ -17,10 +15,10 @@ int main(int argc, char **argv) {
     int fdin2;
     int buf1 = 0;
     int buf2 = 0;
-    int readStatus1;
-    int readStatus2;
+    int readStatus1 = 0;
+    int readStatus2 = 0;
     if (argc != MAX_ARG) {
-        printf("Please pass two files as arguments. Try again");
+        printf("Please pass two files as arguments. Try again\n");
         return 0;
     }
     fdin1 = open(argv[1], O_RDONLY);
@@ -35,40 +33,50 @@ int main(int argc, char **argv) {
         close(fdin1);
         return -1;
     }
-    readStatus1 = (int) (read(fdin1,buf1,1));
+    readStatus1 = (int) read(fdin1, &buf1, 1);
     if (readStatus1 == -1){
-        printf("Error reading fd 1. closing");
+        printf("Error reading fd 1. closing\n");
         closeAll(fdin1, fdin2);
         return -1;
     }
     while (readStatus1 != 0){
+        readStatus2 = (int) read(fdin2,&buf2,1);
         if (readStatus2 == -1){
-            printf("Error reading fd 2. closing");
+            printf("Error reading fd 2. closing\n");
             closeAll(fdin1, fdin2);
             return -1;
         }
         if (readStatus2 == 0){
             //files are not the same - file 2 is shorter
+            printf("1\n");
+            closeAll(fdin1,fdin2);
             return 1;
         }
         //check if bytes are the same
         if (buf1 != buf2){
             //bytes are not the same, files are diffrent
+            printf("1\n");
+            closeAll(fdin1,fdin2);
             return 1;
         }
         //moving to the next byte
-        readStatus1 = (int) (read(fdin1, buf1, 1));
+        readStatus1 = (int) (read(fdin1, &buf1, 1));
         if (readStatus1 == -1){
-            printf("Error reading fd 1. closing");
+            printf("Error reading fd 1. closing\n");
+            closeAll(fdin1,fdin2);
             return -1;
         }
     }
     //checking that file 2 isn't longer
-    readStatus2 = (int) read(fdin2,buf2,1);
+    readStatus2 = (int) read(fdin2,&buf2,1);
 
     if (readStatus2 != 0){
         //file 2 is longer then file 1
+        printf("1\n");
+        closeAll(fdin1,fdin2);
         return 1;
     }
-    return 2/**/;
+    printf("2\n");
+    closeAll(fdin1,fdin2);
+    return 2;
 }
