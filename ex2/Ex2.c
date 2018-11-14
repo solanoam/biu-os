c23417d9b77c526aa9746b538c6666a07e2ff194
solanoam <noam5456@gmail.com>
2018-11-14 19:38:28 +0200

    progress

--- ex2/Ex2.c
+++ ex2/Ex2.c
@@ -6,8 +6,15 @@
 #include <string.h>
 #define MAX_NAME 32
 
-int EvaluateCode(char* file, int input, int output){
-    //code for compering output
+int CompFiles(int input){
+    if ((OpenFile("comp.out")) == -1) {
+        execlp("gcc", "gcc", "comp.c", "-o", "comp.out");
+    }
+
+}
+
+void GetOutput(char *file, char* input){
+    execlp("%s","%s","<","%s"," >", "temp.out", file, file, input);
 }
 
 void WriteToFile(int* fdin, int grade){
@@ -31,14 +38,15 @@ void WriteToFile(int* fdin, int grade){
     }
 }
 
-int writeGrades(int* fdin, char* file, int compilationStatus, int input, int correctOut){
+int writeGrades(int* fdin, char* file, int compilationStatus, char* input, char* correctOut){
     int grade = -1;
     if (compilationStatus == 0){
         WriteToFile(fdin, grade);
         return 0;
     }
     else{
-        grade = EvaluateCode(file, input, correctOut);
+        GetOutput(file, input);
+        grade =
         WriteToFile(fdin, grade);
         return 1;
     }
@@ -46,12 +54,8 @@ int writeGrades(int* fdin, char* file, int compilationStatus, int input, int cor
 
 int OpenFile(char* fileName){
     int fd = open(fileName,O_RDONLY);
-    if (fd < 0){
-        return -1;
-    }
-    else{
-        return fd;
-    }
+    if (fd < 0){return -1;}
+    else{return fd;}
 }
 
 int CompileFile(char* in,int nameLength){
@@ -59,10 +63,10 @@ int CompileFile(char* in,int nameLength){
     strcpy(out, in);
     strcpy(in+nameLength,".c\0");
     strcpy(out+nameLength,".out\0");
-    printf("%s",in);
-    printf("\n");
-    printf("%s",out);
-    printf("\n");
+//    printf("%s",in);
+//    printf("\n");
+//    printf("%s",out);
+//    printf("\n");
     execlp("gcc","gcc","%s","-o","%s",in,out);
     int fdNewFile = open(out, O_RDONLY) == -1;
     if(fdNewFile == -1){
@@ -81,23 +85,22 @@ int main(int argc, char** argv) {
     char buffer;
     int nameLength = 0;
     int compilationStatus;
-
     if (argc != 4){
         printf("Argument count is not correct. terminating...");
         return 0;
     }
     fdin1 = OpenFile(argv[1]);
     if (fdin1 == -1){return 0;}
-    fdin2 = OpenFile(argv[2]);
-    if (fdin2 == -1){close(fdin1); return 0;}
-    fdin3 = OpenFile(argv[3]);
-    if (fdin3 == -1){close(fdin1); close(fdin2); return 0;}
+//    fdin2 = OpenFile(argv[2]);
+//    if (fdin2 == -1){close(fdin1); return 0;}
+//    fdin3 = OpenFile(argv[3]);
+//    if (fdin3 == -1){close(fdin1); close(fdin2); return 0;}
     while ((read(fdin1,&buffer,1)) != 0){
         cFile[nameLength] = buffer;
         //found space
         if ((int) buffer == 10){
             compilationStatus = CompileFile(cFile,nameLength);
-            writeGrades(&fdin1, cFile, compilationStatus, fdin2, fdin3);
+            writeGrades(&fdin1, cFile, compilationStatus, argv[2], argv[3]);
             nameLength = -1;
         }
         nameLength++;
