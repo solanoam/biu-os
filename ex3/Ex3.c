f515dfb3b9abd9d2e1c44b80078379d0211662cf
solanoam <noam5456@gmail.com>
2018-12-16 15:44:14 +0200

    work in progress

--- ex3/Ex3.c
+++ ex3/Ex3.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 
 #define MAX_PROC_ALLOWED 32
+#define MAX_TIME_ARRIVE 4096
 
 struct Proccess{
     int pid;
@@ -15,12 +16,14 @@ struct Proccess{
     int timeBurst;
     int waitTime;
     int timeLeft;
+    int lastStartTime;
     int finished;
 };
 
 void printWaitTime(struct Proccess inputProc[], int numOfProcs){
-    for (int i = 0; i < numOfProcs; i++){
-        printf("Process %d waiting time: %d", inputProc[i].pid, inputProc[i].waitTime);
+    int i;
+    for (i = 0; i < numOfProcs; i++){
+        printf("Process %d waiting time: %d\n", inputProc[i].pid, inputProc[i].waitTime);
     }
 }
 
@@ -28,11 +31,11 @@ void fcfsSim(struct Proccess inputProc[], int numOfProcs){
     int currentProc = 0;
     int timePassed = 0;
     int procsLeft = numOfProcs;
-    printf("Welcome to FCFS simulation!\n");
     while (procsLeft != 0){
-        int min;
-        for (int i = 0; i % numOfProcs; i++){
-            if (!inputProc[i].finished){
+        int min = MAX_TIME_ARRIVE;
+        int i;
+        for (i = 0; i < numOfProcs; i++){
+            if (!(inputProc[i].finished)){
                 if (inputProc[i].timeOfArrivel < min){
                     currentProc = i;
                     min = inputProc[i].timeOfArrivel;
@@ -41,51 +44,184 @@ void fcfsSim(struct Proccess inputProc[], int numOfProcs){
         }
         inputProc[currentProc].finished = 1;
         inputProc[currentProc].timeLeft = 0;
-        inputProc[currentProc].waitTime =+ timePassed;
+        inputProc[currentProc].waitTime += timePassed-inputProc[currentProc].timeOfArrivel;
         printf("#%d:[%d]", inputProc[currentProc].pid, timePassed);
-        timePassed =+ inputProc[currentProc].timeBurst;
+        timePassed += inputProc[currentProc].timeBurst;
         printf("-[%d]\n", timePassed);
         procsLeft--;
+        min = MAX_TIME_ARRIVE;
+    }
+}
+/*
+void rrSim(struct Proccess inputProc[], int numOfProcs, int frame){
+    int currentProc = 0;
+    int oldProc = 0;
+    int lastProc = 0;
+    int timePassed = 0;
+    int procsLeft = numOfProcs;
+    int currentJob = 0;
+    while (procsLeft) {
+        int i;
+        int count = 0;
+        int chosen = 0;
+        for (i = currentProc + 1; count < numOfProcs; i = i % numOfProcs) {
+            if (i == numOfProcs) { i = 0; }
+            if (!inputProc[i].finished && !(currentJob % frame) && !(chosen)) {
+                if (inputProc[i].timeOfArrivel <= timePassed + currentJob) {
+                    oldProc = currentProc;
+                    currentProc = i;
+                    chosen = 1;
+                }
+            }
+            if (i == numOfProcs - 1 && !(currentJob % frame) && (oldProc != currentProc)) {
+                if (currentJob != 0) {
+                    printf("#%d:[%d]-[%d]\n", inputProc[oldProc].pid, timePassed, timePassed + currentJob);
+                    timePassed += currentJob;
+                    currentJob = 0;
+                }
+            }
+            i++;
+            count++;
+        }
+        if (!(currentJob % frame) && chosen) {
+            inputProc[currentProc].timeLeft--;
+            currentJob++;
+        }
+        else {
+            timePassed++;
+        }
+        if (!inputProc[currentProc].timeLeft) {
+            printf("#%d:[%d]-[%d]\n", inputProc[currentProc].pid, timePassed, timePassed + currentJob);
+            timePassed += currentJob;
+            currentJob = 0;
+            procsLeft--;
+            inputProc[currentProc].finished = 1;
+            inputProc[currentProc].waitTime = timePassed - inputProc[currentProc].timeOfArrivel - inputProc[currentProc].timeBurst;
+            int i;
+            for (i = 0; i < numOfProcs; i++){
+                if (!(inputProc[i].finished)) {currentProc = i;}
+            }
+        }
+    }
+}*/
+void rrSim(struct Proccess inputProc[], int numOfProcs, int frame){
+    int queue[MAX_PROC_ALLOWED];
+    int currentProc = 0;
+    int timePassed = 0;
+    int procsLeft = numOfProcs;
+    int oldProc = 0;
+    while (procsLeft != 0) {
+        int i;
+        int queueLen = 0;
+        int timeFrameLeft = 0;
+        for (i = currentProc+1; i < numOfProcs; i = i % numOfProcs) {
+            i = i % numOfProcs;
+            if (!(inputProc[i].finished) && inputProc[i].timeOfArrivel <= timePassed) {
+                queue[queueLen] = i;
+                queueLen++;
+            }
+            i++;
+        }
+        int j;
+        for (j = 0; j < queueLen; j++){
+            currentProc = queue[j];
+            if (oldProc != currentProc && !(inputProc[currentProc].finished) && (queueLen)){
+                printf("#%d:[%d]-[%d]\n", inputProc[oldProc].pid, inputProc[oldProc].lastStartTime , timePassed);
+                inputProc[currentProc].lastStartTime = timePassed;
+            }
+            if (!(inputProc[currentProc].finished)) {
+                inputProc[currentProc].timeLeft -= frame;
+                if (inputProc[currentProc].timeLeft <= 0){
+                    inputProc[currentProc].finished = 1;
+                    procsLeft--;
+                    timeFrameLeft = inputProc[currentProc].timeLeft;
+                    printf("#%d:[%d]-[%d]\n", inputProc[oldProc].pid, inputProc[oldProc].lastStartTime , timePassed + frame + timeFrameLeft);
+                    inputProc[currentProc].waitTime = timePassed + frame + timeFrameLeft - inputProc[currentProc].timeOfArrivel - inputProc[currentProc].timeBurst;
+                }
+            }
+            oldProc = currentProc;
+        }
+        if (queueLen) {
+            timePassed += frame + timeFrameLeft;
+        }
+        else {
+            timePassed++;
+        }
+        for (j = 0; j < queueLen; j++) {
+            queue[j] = 0;
+        }
     }
 }
 
-void rrSim(struct Proccess inputProc[], int numOfProcs){
-    printf("Welcome to RR simulation!\n");
-}
-
 void sjfSim(struct Proccess inputProc[], int numOfProcs){
-    printf("Welcome to SJF simulation!\n");
+    int currentProc = 0;
+    int oldProc = 0;
+    int timePassed = 0;
+    int procsLeft = numOfProcs;
+    int currentJob = 0;
+    while (procsLeft) {
+        int i;
+        for (i = 0; i < numOfProcs; i++) {
+            if (!inputProc[i].finished && !(currentJob % 1)){
+                if (inputProc[i].timeOfArrivel <= timePassed+currentJob && inputProc[currentProc].timeLeft >= inputProc[i].timeLeft){
+                    oldProc = currentProc;
+                    currentProc = i;
+                }
+            }
+            if (i == numOfProcs - 1 && !(currentJob % 1) && (oldProc != currentProc)){
+                if (currentJob != 0) {
+                    printf("#%d:[%d]-[%d]\n", inputProc[oldProc].pid, timePassed, timePassed + currentJob);
+                    timePassed += currentJob;
+                    currentJob = 0;
+                }
+            }
+        }
+        inputProc[currentProc].timeLeft--;
+        currentJob++;
+        if (!inputProc[currentProc].timeLeft) {
+            printf("#%d:[%d]-[%d]\n", inputProc[currentProc].pid, timePassed, timePassed + currentJob);
+            timePassed += currentJob;
+            currentJob = 0;
+            procsLeft--;
+            inputProc[currentProc].finished = 1;
+            inputProc[currentProc].waitTime = timePassed - inputProc[currentProc].timeOfArrivel - inputProc[currentProc].timeBurst;
+            int i;
+            for (i = 0; i < numOfProcs; i++){
+                if (!(inputProc[i].finished)) {currentProc = i;}
+            }
+        }
+    }
 }
 
 void startSim(int usrChioceSim, struct Proccess inputProc[], int numOfProcs){
+    int frame;
     switch (usrChioceSim){
         case 1:
             fcfsSim(inputProc, numOfProcs);
             break;
         case 2:
-            rrSim(inputProc, numOfProcs);
+            scanf("%d", &frame);
+            rrSim(inputProc, numOfProcs, frame);
             break;
         case 3:
             sjfSim(inputProc, numOfProcs);
             break;
-        defult:
-            printf("simulation do not exsist. terminating...");
+        default:
             exit(0);
     }
     printWaitTime(inputProc, numOfProcs);
 }
 
-int getProccess(struct Proccess inputProc[]){
+int getProccess(struct Proccess* inputProc){
     int numOfProcs = 0;
-    printf("Please insert the number of process that will be collected");
     scanf("%d", &numOfProcs);
-    for (int i = 0; i != numOfProcs; i--){
-        inputProc[i].pid == i+1;
+    int i;
+    for (i = 0; i != numOfProcs; i++){
+        inputProc[i].pid = i+1;
         inputProc[i].finished = 0;
         inputProc[i].waitTime = 0;
-        printf("Please insert time of arrival:\n");
+        inputProc[i].lastStartTime = 0;
         scanf("%d", &(inputProc[i].timeOfArrivel));
-        printf("Please insert time burst:\n");
         scanf("%d", &(inputProc[i].timeBurst));
         inputProc[i].timeLeft = inputProc[i].timeBurst;
     }
@@ -94,10 +230,6 @@ int getProccess(struct Proccess inputProc[]){
 
 int getSimType(){
     int usrChoice = 0;
-    printf("Please choose a simulations:\n");
-    printf("for FCFS press 1\n");
-    printf("for RR press 2\n");
-    printf("for SJF press 3\n");
     scanf("%d",&usrChoice);
     return usrChoice;
 }
