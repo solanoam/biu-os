a9cb27f22a6d03c7e4a123c2d9a58b5011a51150
solanoam <noam5456@gmail.com>
2019-01-04 10:38:59 -0800

    code is finished

--- ex4/Ex41.c
+++ ex4/Ex41.c
@@ -5,10 +5,16 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+//macros
 #define READ 0
 #define WRITE 1
 #define EXIT_BUTTON 'q'
+#define RIGHT_BUTTON 'd'
+#define LEFT_BUTTON 'a'
+#define DOWN_BUTTON 's'
+#define SWAP_BUTTON 'w'
 
+//given function getch
 char getch(){
     char buf=0;
     struct termios old={0};
@@ -31,12 +37,13 @@ char getch(){
     return buf;
 }
 
+// exit gracfully
 void exitGracfully(int pipe[], int pid, int status){
     close(pipe[WRITE]);
     if (pid != 0){kill(pid, SIGKILL);}
     exit(status);
 }
-
+//handling fork error
 int handleForkError(int pipe[], int pid){
     if (pid < 0){ //check if error happened
         exitGracfully(pipe, 0, -1);
@@ -45,38 +52,36 @@ int handleForkError(int pipe[], int pid){
 }
 
 //function forks and runs draw proc
-//TODO handling errors
 int runGame(int pipe[]){
-    int childId = fork();
-    printf("%d", childId);
-    if (childId == 0){
+    int childId = fork(); //forking
+    if (childId == 0){ //child
         close(pipe[WRITE]);
         dup2(pipe[READ],0);
         close(pipe[READ]);
         execl("draw.out", "draw.out", NULL);
-    }
-    handleForkError(pipe, childId);
+    } 
+    handleForkError(pipe, childId); //father
     return childId;
-}
-void writeKey(char keyPressed){
-    printf("%c", keyPressed);
 }
 
 //handeling key presses
 void handleKeys(int pid, int* pipe){
     char keyPressed;
     while (true) {
-        keyPressed = getch();
-        write(pipe[WRITE], &keyPressed, 1);
-        if (keyPressed == EXIT_BUTTON) {exitGracfully(pipe, pid, 0); exit(-1);}
-        kill(pid, SIGUSR2);
+        keyPressed = getch(); //getting key press from user
+        //if it's a relevant key
+        if (keyPressed == RIGHT_BUTTON || keyPressed == LEFT_BUTTON || keyPressed == DOWN_BUTTON || keyPressed == SWAP_BUTTON || keyPressed == EXIT_BUTTON){
+            write(pipe[WRITE], &keyPressed, 1);
+            if (keyPressed == EXIT_BUTTON) {exitGracfully(pipe, pid, 0); exit(-1);}
+            kill(pid, SIGUSR2);
+        }
     }
 }
 
 int main() {
     int fd_pipe[2];
-    pipe(fd_pipe);
-    int childId = runGame(fd_pipe);
+    pipe(fd_pipe); //create pipe
+    int childId = runGame(fd_pipe); //start draw.out
     handleKeys(childId, fd_pipe);
     return 0;
 }
--- ex4/Ex42.c
+++ ex4/Ex42.c
@@ -10,51 +10,57 @@
 #include <stdbool.h>
 #define HORI 0
 #define VERT 1
+#define RIGHT_BUTTON 'd'
+#define LEFT_BUTTON 'a'
+#define DOWN_BUTTON 's'
+#define SWAP_BUTTON 'w'
 
 //globals
 int posX = 9;
 int posY = 0;
 int blockState = HORI;
 
+//
 void fallingBlock(int sig);
 
-
+// validation for new x position
 int validatorX(int currPosX, int currBlockState){
-    if (currPosX == 17 && currBlockState == HORI){return posX;}
+    if (currPosX == 16 && currBlockState == HORI){return posX;}
     if (currPosX == 1 && currBlockState == HORI){return posX;}
-    if (currPosX == 19 && currBlockState == VERT){return posX;}
-    if (currPosX == 1 && currBlockState == VERT){return posX;}
+    if (currPosX == 17 && currBlockState == VERT){return posX;}
+    if (currPosX == 0 && currBlockState == VERT){return posX;}
     return currPosX;
 }
-
+// validation for new y position
 int validatorY(int currPosY, int currentBlockState){
     if (currPosY == 20 && currentBlockState == HORI){return 0;}
     if (currPosY == 19 && currentBlockState == VERT){return 0;}
     return currPosY;
 }
+// validation for new block state
 int validatorFlip(int currPosX, int currPosY, int currentBlockState){
-    if (currPosY == 19 && currentBlockState == HORI){return blockState;}
-    if (currPosX == 19 && currentBlockState == VERT){return blockState;}
-    if (currPosX == 1 && currentBlockState == VERT){return blockState;}
+    if (currPosY == 17 && currentBlockState == VERT){return blockState;}
+    if (currPosX >= 16 && currentBlockState == HORI){return blockState;}
+    if (currPosX == 1 && currentBlockState == HORI){return blockState;}
     return currentBlockState;
 }
-
+//handling new position and state for the block
 void newPosHandler(int currPosX, int currPosY, int currBlockState){
     posX = validatorX(currPosX, currBlockState);
     posY = validatorY(currPosY, currBlockState);
     blockState = validatorFlip(currPosX, currPosY, currBlockState);
 }
-
+//print the floor for the board
 void printFloor(){
     printf("********************\n");
 }
-
+//print the block if the block is vertical
 void printLineBlockVert(int posX){
     printf("*");
     int i;
     for (i = 0; i < 18; i++) {
-        if (i == posX){
-            printf("|");
+        if (i == posX){ //only when reachig the block draw it
+            printf("-");
         }
         else{
             printf(" ");
@@ -62,18 +68,18 @@ void printLineBlockVert(int posX){
     }
     printf("*\n");
 }
-
+//print regular line without block
 void printLine(){
     printf("*                  *\n");
 }
-
+//print the block if the block is vertical
 void printLineBlockHori(int posX){
     printf("*");
     int i;
     for (i = 0; i < 18; i++) {
-        if (i == posX -1){
+        if (i == posX -1){ //only when reachig the block draw it
             printf("---");
-            i += 2;
+            i += 2; //we have printed 3 chars
         }
         else{
             printf(" ");
@@ -81,71 +87,78 @@ void printLineBlockHori(int posX){
     }
     printf("*\n");
 }
-
+//handle printing the entire board
 void printBoard(int currPosX, int currPosY, int currBlockState){
     int i;
-    system("clear");
+    system("clear"); //clear the screen
     for (i = 0; i < 20; i++){
-        if (currBlockState == HORI){
-            if (currPosY != i){
-                printLine();
-            }
-            else {
-                printLineBlockHori(currPosX);
-            }
-        }
-        else {
-            if (i == currPosY -1 || i == currPosY || i == currPosY + 1){
-                printLineBlockVert(currPosX);
-            }
-            else {
-                printLine();
-            }
+        switch (currBlockState){
+            case HORI: //call horizotal function if block is horizontal
+                if (currPosY != i){
+                    printLine();
+                }
+                else { //line where the block is at
+                    printLineBlockHori(currPosX);
+                }
+                break;
+            case VERT: //call vertical function if block is vertical
+                if (i == currPosY -1 || i == currPosY || i == currPosY + 1){ //line where the block is at
+                    printLineBlockVert(currPosX);
+                }
+                else {
+                    printLine();
+                }
+                break;    
         }
     }
     printFloor();
 }
-
+//reest the alarm once it's goes off
 void resetAlarm(){
-    signal(SIGALRM,fallingBlock);
+    signal(SIGALRM,fallingBlock); 
     alarm(1);
 }
 
 void keyHandler(int sig){
-    //TODO handling read error
     char key;
-    read(0,&key,sizeof(char));
-    switch (key){
-        case 'a':
+    read(0,&key,sizeof(char)); //getting char in pipe
+    switch (key){ //for each key, evaluate next step. handler will set the a new value if needed
+        case LEFT_BUTTON:
             newPosHandler(posX - 1, posY, blockState);
             break;
-        case 's':
+        case DOWN_BUTTON:
             newPosHandler(posX, posY + 1, blockState);
             break;
-        case 'd':
+        case RIGHT_BUTTON:
             newPosHandler(posX + 1, posY, blockState);
             break;
-        case 'w':
-            if (blockState == HORI){newPosHandler(posX, posY, VERT);}
-            if (blockState == VERT){newPosHandler(posX, posY, HORI);}
+        case SWAP_BUTTON:
+            switch (blockState){
+                case HORI:
+                    newPosHandler(posX, posY, VERT);
+                    break;
+                case VERT:
+                    newPosHandler(posX,posY,HORI);
+                    break;
+            }
             break;
         default:
             break;
     }
-    signal(SIGUSR2,keyHandler);
+    signal(SIGUSR2,keyHandler); //reseting listening to the signal
 }
-
-void fallingBlock(int sig){
+//moving the block down if the alarm went off
+void fallingBlock(int sig){ 
     newPosHandler(posX, posY + 1, blockState);
     resetAlarm();
 }
 
 int main(){
-    printBoard(posX, posY, blockState);
-    signal(SIGUSR2,keyHandler);
-    resetAlarm();
+    printBoard(posX, posY, blockState); //print board
+    signal(SIGUSR2,keyHandler); //listen to signal from father
+    resetAlarm(); //reset the alarm
     while(true) {
-        printBoard(posX, posY, blockState);
-        pause();
+        printBoard(posX, posY, blockState); //print board
+        pause(); //wait to signal - only function that run now are handlers and printboard each signal
     }
 }
